b66cae4c82c20de9f394809135b55f92f8044bfb
sarahaq12 <142425123+sarahaq12@users.noreply.github.com>
2023-08-16 18:03:14 +0300

    Add files via upload

---
+++ Pages/register.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class registerModel : PageModel
+    {
+        private readonly ILogger<registerModel> _logger;
+
+        public registerModel(ILogger<registerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
